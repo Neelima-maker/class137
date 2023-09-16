4d25e6fabd43ac5f1a62fd3d2996bfe1291594e9
Neelima-maker <74758622+Neelima-maker@users.noreply.github.com>
2023-09-16 16:36:45 +0530

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,56 @@
+#Text Data Preprocessing Lib
+import nltk
+
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+
+import json
+
+words=[]
+classes=[]
+word_tags_list=[]
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+
+train_data_file = open("intents.json").read()
+
+intents = json.loads(train_data_file)
+
+# function for appending stem words
+def get_stem_words(words,ignore_words):
+        stem_words = []
+        for word in words:
+                if word not in ignore_words:
+                        w = stemmer.stem(word.lower())
+                        stem_words.append(w)
+
+        return stem_words
+
+
+
+
+    
+        # Add all words of patterns to list
+      
+        # Add all tags to the classes list
+        
+#print(stem_words)
+#print(word_tags_list[0]) 
+#print(classes)   
+
+#Create word corpus for chatbot
+
+
+#print(stem_words)
+#print(classes) 
+
+
+
+
+
+
+
+
+
+
+
+
